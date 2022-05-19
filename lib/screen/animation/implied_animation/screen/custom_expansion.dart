3b2b0eb2f28d9d40ac13f6392860b2e881db3db0
montonurb <brunomoncar007@gmail.com>
2022-05-18 22:43:21 -0300

    adicionando segundo exercicio de animação implícita

---
+++ lib/screen/animation/implied_animation/screen/custom_expansion.dart
@@ -0,0 +1,94 @@
+// ignore_for_file: must_be_immutable
+
+import 'package:flutter/material.dart';
+
+class CustomExpansion extends StatefulWidget {
+  Map texts;
+  int index;
+
+  CustomExpansion({Key? key, required this.texts, required this.index})
+      : super(key: key);
+
+  @override
+  State<CustomExpansion> createState() => _CustomExpansionState();
+}
+
+class _CustomExpansionState extends State<CustomExpansion> {
+  double turns = 0.0;
+  Color colorButton = Colors.black;
+  double heightFactor = 0.0;
+  bool onPressed = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          if (!onPressed) {
+            turns = 0.5;
+            colorButton = Colors.blue;
+            heightFactor = 1.0;
+            onPressed = true;
+          } else {
+            heightFactor = 0.0;
+            turns = 0.0;
+            colorButton = Colors.black;
+            onPressed = false;
+          }
+        });
+      },
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 2.0),
+            child: Container(
+              height: 50,
+              color: Colors.white,
+              child: Row(
+                children: [
+                  Text(
+                    "${widget.texts["title"]} ${widget.index}",
+                    style: TextStyle(
+                      color: colorButton,
+                      fontSize: 18,
+                    ),
+                  ),
+                  const Spacer(),
+                  AnimatedRotation(
+                    turns: turns,
+                    duration: const Duration(seconds: 1),
+                    child: Icon(
+                      Icons.keyboard_arrow_down,
+                      color: colorButton,
+                      size: 30,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          ClipRRect(
+            child: AnimatedAlign(
+              duration: const Duration(seconds: 1),
+              alignment: Alignment.center,
+              heightFactor: heightFactor,
+              child: Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                child: Column(
+                  children: [
+                    const FlutterLogo(size: 75),
+                    Text(
+                      "${widget.texts["textBody"]}",
+                      textAlign: TextAlign.justify,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screen/animation/implied_animation/second_implied_animation_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:masterclass_app/models/implied_animation/text_model.dart';
+import 'package:masterclass_app/screen/animation/implied_animation/screen/custom_expansion.dart';
+
+class SecondImpliedAnimationScreen extends StatefulWidget {
+  const SecondImpliedAnimationScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SecondImpliedAnimationScreen> createState() =>
+      _SecondImpliedAnimationScreenState();
+}
+
+class _SecondImpliedAnimationScreenState
+    extends State<SecondImpliedAnimationScreen> {
+  List texts = listText;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("MyScrollView"),
+      ),
+      body: ListView.builder(
+          itemCount: texts.length,
+          itemBuilder: (BuildContext context, int index) {
+            return CustomExpansion(texts: texts[index], index: index);
+          }),
+    );
+  }
+}
