42e8082c5106b8296bb2f6f7cc69ca8dc8eec757
montonurb <brunomoncar007@gmail.com>
2022-05-18 22:18:51 -0300

    adicionando novas rotas no card

---
+++ lib/models/exercise/card.dart
@@ -0,0 +1,43 @@
+List<List<dynamic>> card = const [
+  [
+    "assets/img/home/awesome-running.svg",
+    "Design Patterns I",
+    1,
+    "Introdução aos estudos de Design Pattern. MVC, MVP e MVW",
+    "",
+    [
+      "/cpf-design-pattern-screen",
+    ],
+  ],
+  [
+    "assets/img/home/awesome-running.svg",
+    "Animações",
+    2,
+    "Estudos sobre animações implícitas e controladas, contendo 4 exercícios e 2 estudos.",
+    "",
+    [
+      "/implied-animation-screen",
+      "/controlled-animation-screen",
+    ],
+  ],
+  [
+    "assets/img/home/awesome-glasses.svg",
+    "Leitura de Mockup",
+    2,
+    "Aplicação da técnica de leitura de mockup, contendo 2 exercícios.",
+    "",
+    [
+      "/bank-mockup-screen",
+      "/tinder-mockup-screen",
+      3,
+      4,
+    ],
+  ],
+  [
+    "assets/img/home/material-toys.svg",
+    "Playground",
+    0,
+    "Ambiente destinado a testes e estudos em geral.",
+    "",
+  ],
+];
