e865a8668fa9a22b1c3157a27d5162cbfaddaece
montonurb <brunomoncar007@gmail.com>
2022-05-18 22:22:42 -0300

    adicionando mascara no input do cpf e logica para remover os pontos e traço da mascara

--- lib/models/validation_cpf/cpf.dart
+++ lib/models/validation_cpf/cpf.dart
@@ -6,45 +6,41 @@ class Cpf {
   Cpf({required this.cpf});
 
   bool validationCpf(String cpf) {
+    //COM O \W (W MAIUSCULO) ELE REMOVE OS CARACTERES ESPECIAIS
+    var newCpf = cpf.replaceAll(RegExp(r'[\W]'), '');
+    print("NOVOCPF: $newCpf");
     int total = 0;
     bool validation = false;
-    if (cpf.length != 11) {
+    if (newCpf.length != 11) {
       return false;
     } else {
 //STEP 1:
-      print("STEP 1:");
       var j = 10;
       for (var i = 0; i <= 8; i++) {
-        int num = int.parse(cpf[i]);
+        int num = int.parse(newCpf[i]);
         var value = num * j;
         total += value;
-        print("$num * $j = $total");
         j--;
       }
 
       total *= 10;
       total %= 11;
       total = total == 10 ? 0 : total;
-      print("TESTE: $total");
-      validation = total == int.parse(cpf[9]) ? true : false;
+      validation = total == int.parse(newCpf[9]) ? true : false;
 
       //STEP 2
-      print("STEP 2:");
       total = 0;
       j = 11;
       for (var i = 0; i <= 9; i++) {
-        int num = int.parse(cpf[i]);
+        int num = int.parse(newCpf[i]);
         var value = num * j;
         total += value;
-        print("$num * $j = $total");
         j--;
       }
       total *= 10;
       total %= 11;
       total = total == 10 ? 0 : total;
-      print("TESTE: $total");
-      validation = total == int.parse(cpf[10]) ? true : false;
-      print("TOTAL2 $total");
+      validation = total == int.parse(newCpf[10]) ? true : false;
 
       return validation == true ? true : false;
     }
---
+++ lib/screen/design_pattern/cpf/cpf_screen.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:masterclass_app/models/validation_cpf/cpf.dart';
+import 'package:masterclass_app/theme/app_theme.dart';
+import 'package:masterclass_app/widgets/custom_header.dart';
+import 'package:mask_input_formatter/mask_input_formatter.dart';
+
+class CpfScreen extends StatefulWidget {
+  const CpfScreen({Key? key}) : super(key: key);
+
+  @override
+  State<CpfScreen> createState() => _CpfScreenState();
+}
+
+class _CpfScreenState extends State<CpfScreen> {
+  late Cpf cpf = Cpf(cpf: '');
+  bool validation = false;
+  String mensagem = '';
+  TextEditingController controller = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: AppTheme.colors.scaffoldBackground,
+        body: Padding(
+          padding:
+              const EdgeInsets.only(top: 35, left: 15, right: 15, bottom: 10),
+          child: Column(
+            children: [
+              CustomHeader(title: "Validador de CPF", logo: false),
+              const SizedBox(height: 16),
+              TextField(
+                style: TextStyle(
+                  fontSize: 20,
+                  color: AppTheme.colors.textHighlight,
+                ),
+                keyboardType: TextInputType.number,
+                inputFormatters: [MaskInputFormatter(mask: '###.###.###-##')],
+                decoration: InputDecoration(
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: AppTheme.colors.textHighlight,
+                    ),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: AppTheme.colors.textHighlight,
+                    ),
+                  ),
+                  labelText: 'CPF:',
+                  labelStyle: TextStyle(color: AppTheme.colors.textHighlight),
+                  prefixIcon: validation == true
+                      ? Icon(
+                          Icons.check,
+                          color: AppTheme.colors.check,
+                        )
+                      : Icon(
+                          Icons.error_outline,
+                          color: AppTheme.colors.primary,
+                        ),
+                ),
+                controller: controller,
+              ),
+              const SizedBox(height: 10),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: AppTheme.colors.primary,
+                ),
+                onPressed: () {
+                  if (controller.text != '') {
+                    setState(() {
+                      validation = cpf.validationCpf(controller.text);
+                      mensagem =
+                          validation == true ? "CPF válido!" : "CPF inválido!";
+                    });
+                  } else {
+                    setState(() {
+                      mensagem = "Informe o CPF!";
+                    });
+                  }
+                },
+                child: Text(
+                  "Validar CPF!",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: AppTheme.colors.textHighlight,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 50),
+              Text(
+                mensagem,
+                style: TextStyle(
+                  fontSize: 24,
+                  color: mensagem == "CPF válido!"
+                      ? AppTheme.colors.check
+                      : AppTheme.colors.textHighlight,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
