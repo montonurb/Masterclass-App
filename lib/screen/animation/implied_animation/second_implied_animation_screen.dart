75c75dc10eed4773319904b59b8ff0545a4b201b
Bruno Monteiro <brunomoncar007@gmail.com>
2022-07-18 10:23:51 -0300

    ajustes no layout da animação implícita

--- lib/screen/animation/implied_animation/second_implied_animation_screen.dart
+++ lib/screen/animation/implied_animation/second_implied_animation_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:masterclass_app/models/animation/text_model.dart';
 import 'package:masterclass_app/screen/animation/implied_animation/second_implied_custom_expansion.dart';
+import 'package:masterclass_app/screen/widgets/header_widget.dart';
+import 'package:masterclass_app/theme/app_theme.dart';
 
 class SecondImpliedAnimationScreen extends StatefulWidget {
   const SecondImpliedAnimationScreen({Key? key}) : super(key: key);
@@ -17,14 +19,24 @@ class _SecondImpliedAnimationScreenState
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("MyScrollView"),
+      backgroundColor: AppTheme.colors.scaffoldBackground,
+      body: Padding(
+        padding:
+            const EdgeInsets.only(top: 60, left: 15, right: 15, bottom: 10),
+        child: Column(
+          children: [
+            HeaderWidget(title: "Animação Implícita", logo: false),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.85,
+              child: ListView.builder(
+                  itemCount: texts.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return CustomExpansion(texts: texts[index], index: index);
+                  }),
+            )
+          ],
+        ),
       ),
-      body: ListView.builder(
-          itemCount: texts.length,
-          itemBuilder: (BuildContext context, int index) {
-            return CustomExpansion(texts: texts[index], index: index);
-          }),
     );
   }
 }
